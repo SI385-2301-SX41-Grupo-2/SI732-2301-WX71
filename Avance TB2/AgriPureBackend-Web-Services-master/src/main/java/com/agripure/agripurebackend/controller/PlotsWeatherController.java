334c44c8c93b81bba6525e18fd51ebc8b5555f65
Erick Armando Cueva Elera <89040344+ECueva1809@users.noreply.github.com>
2023-06-07 00:04:01 -0500

    Update PlotsWeatherController.java

--- Avance TB2/AgriPureBackend-Web-Services-master/src/main/java/com/agripure/agripurebackend/controller/PlotsWeatherController.java
+++ Avance TB2/AgriPureBackend-Web-Services-master/src/main/java/com/agripure/agripurebackend/controller/PlotsWeatherController.java
@@ -2,6 +2,8 @@ package com.agripure.agripurebackend.controller;
 
 import com.agripure.agripurebackend.entities.PlotsWeather;
 import com.agripure.agripurebackend.service.IPlotsWeatherService;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,41 +12,49 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Optional;
-
+/**
+ * Comment.
+ */
 @RestController
 @RequestMapping("/api/plotsweather")
 public class PlotsWeatherController {
-    private IPlotsWeatherService plotsWeatherService;
+  private IPlotsWeatherService plotsWeatherService;
 
-    public PlotsWeatherController(IPlotsWeatherService plotsWeatherService) {
-        this.plotsWeatherService = plotsWeatherService;
-    }
+  public PlotsWeatherController(IPlotsWeatherService plotsWeatherService) {
+    this.plotsWeatherService = plotsWeatherService;
+  }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<PlotsWeather>>findAllPlotsWeather() {
-        try {
-            List<PlotsWeather> plotsWeathers = plotsWeatherService.getAll();
-            if (plotsWeathers.size() > 0)
-                return new ResponseEntity<>(plotsWeathers, HttpStatus.OK);
-            else
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+  /**
+   * Comment.
+   */
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<PlotsWeather>> findAllPlotsWeather() {
+    try {
+      List<PlotsWeather> plotsWeathers = plotsWeatherService.getAll();
+      if (plotsWeathers.size() > 0) {
+        return new ResponseEntity<>(plotsWeathers, HttpStatus.OK);
+      } else {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+      }
+    } catch (Exception e) {
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
+  }
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<PlotsWeather>findPlotsWeatherById(@PathVariable("id") Long id) {
-        try {
-            Optional<PlotsWeather> plotsWeather = plotsWeatherService.getById(id);
-            if (!plotsWeather.isPresent())
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            else
-                return new ResponseEntity<>(plotsWeather.get(), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+  /**
+   * Comment.
+   */
+  @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<PlotsWeather> findPlotsWeatherById(@PathVariable("id") Long id) {
+    try {
+      Optional<PlotsWeather> plotsWeather = plotsWeatherService.getById(id);
+      if (!plotsWeather.isPresent()) {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+      } else {
+        return new ResponseEntity<>(plotsWeather.get(), HttpStatus.OK);
+      }
+    } catch (Exception e) {
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
+  }
 }
